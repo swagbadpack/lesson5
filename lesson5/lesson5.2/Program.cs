dc040044fb32481a84702cae86e6d2149f70e0f9
swagbadpack <sharovmikh@yandex.ru>
2022-02-01 15:58:13 +0300

    lesson5

---
+++ lesson5/lesson5.2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson5._2
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Разделение входной строки на слова
+        /// </summary>
+        /// <param name="inputPhrase"></param>
+        /// <returns></returns>
+        static string[] Divider(string inputPhrase)
+        {
+            string[] end = inputPhrase.Split(' ');
+            return end;
+            
+        }
+        /// <summary>
+        /// Вывод строки со словами в обратном порядке
+        /// </summary>
+        /// <param name="inputPhrase"></param>
+        /// <returns></returns>
+        static string[] ReversWords(string inputPhrase)        //ResultString += NewString + “ ”  тема для рассуждений
+        {
+            string[] start = Divider(inputPhrase);
+            string[] reverse = new string[start.Length];
+            for (int i = reverse.Length - 1; i + 1 > 0; i--)
+            {
+                int j = 0;
+                reverse[j] = start[i];
+                Console.WriteLine(reverse[j]);
+                j++;
+            }
+            return reverse;
+
+
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите предложение: ");
+            string inputPhrase = Console.ReadLine();
+            ReversWords(inputPhrase);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lesson5/lesson5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson5
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Разделение предложения на массив слов
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        static string[] Divider(string text)
+        {
+            string[] end = text.Split(' ');
+            return end;
+        }
+        /// <summary>
+        /// Вывод отдельных слов на экран
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        static string[] PrintResult(string text)
+        {
+            string[] words = Divider(text);
+            foreach (var element in words)
+            Console.WriteLine(element);
+            return words;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Ведите предложение: ");
+            string text = Console.ReadLine();
+            Divider(text);
+            PrintResult(text);
+            Console.ReadKey();
+        }
+    }
+}
